cf731d872d95820fcdfdb436dfc8de95514ea84c
Bina Akene <akenebina@gmail.com>
2023-02-14 11:19:29 +0100

    add support

---
+++ src/api/password/controllers/password.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index: async ctx => {
+      return 'Hello World!';
+  }
+}
---
+++ src/api/support/controllers/support.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * support controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::support.support', {
+  async create(ctx) {
+    const user = ctx.state.user;
+
+    const donation = await super.create(ctx);
+
+    const updated = await strapi.entityService.update(
+      "api::support.support",
+      donation.data.id,
+      {
+        data: {
+          user: user.id,
+        },
+      }
+    );
+
+    return updated;
+  },
+  async find(ctx) {
+    const user = ctx.state.user;
+
+    ctx.query.filters = {
+      ...(ctx.query.filters || {}),
+      user: user.id,
+    };
+
+    return super.find(ctx);
+  },
+  async findOne(ctx) {
+    const user = ctx.state.user;
+
+    ctx.query.filters = {
+      ...(ctx.query.filters || {}),
+      user: user.id,
+    };
+
+    return super.findOne(ctx);
+  },
+  async update(ctx) {
+    const user = ctx.state.user;
+
+    ctx.query.filters = {
+      ...(ctx.query.filters || {}),
+      user: user.id,
+    };
+
+    return super.update(ctx);
+  },
+  async delete(ctx) {
+    const user = ctx.state.user;
+
+    ctx.query.filters = {
+      ...(ctx.query.filters || {}),
+      user: user.id,
+    };
+
+    return super.delete(ctx);
+  },
+});
